e566807a9656d984cdeb42578f365118b3ef3c45
Kim Thien <thienkimly2601@gmail.com>
2026-07-02 01:04:54 +0700

    fix: update API

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://magnula-home-production.up.railway.app/api';
+
 export const API = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: API_URL,
     withCredentials: true
 });
